6e17c24261f5430ac5c27c785e6fa8c7cbb9696c
derrors <derrors@njust.edu.cn>
2023-12-19 23:28:55 +0800

    feat: 初始化项目

---
+++ src/main/java/com/derrors/cn/PtToolsApplication.java
@@ -0,0 +1,17 @@
+package com.derrors.cn;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class PtToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtToolsApplication.class, args);
+        log.info("Pt-Tools Start Success！");
+    }
+
+}
---
+++ src/test/java/com/derrors/cn/PtToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derrors.cn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
