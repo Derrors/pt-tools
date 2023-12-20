f8a351cfff2b00ed2dcc238a681ee2a67d6f97d3
liqinghe.lqh <liqinghe.lqh@bytedance.com>
2023-12-20 11:08:01 +0800

    feat: 初始化项目配置

--- tools-app/src/main/java/com/derrors/pt/tools/app/PtToolsApplication.java
+++ tools-app/src/main/java/com/derrors/pt/tools/app/PtToolsApplication.java
@@ -1,12 +1,16 @@
-package com.derrors.cn;
+package com.derrors.pt.tools.app;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @Slf4j
-@SpringBootApplication
+@MapperScan("com.derrors.pt.tools.data.mapper")
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class PtToolsApplication {
 
     public static void main(String[] args) {
